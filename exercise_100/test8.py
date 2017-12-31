4fd0359c0ecf6858a36172d21f350029d37b799d
luyaJ <1579618152@qq.com>
2017-12-31 23:13:56 +0800

    九九乘法表

---
+++ exercise_100/test8.py
@@ -0,0 +1,7 @@
+# 输出 9*9 乘法口诀表
+
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print("%d*%d=%d" % (j, i, j*i), "\t", end="")
+        # print(j, "*", i, "=", i*j, "\t", end="")
+    print()
