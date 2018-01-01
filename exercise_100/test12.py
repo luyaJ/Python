ddd10a9b158e31e2ed51ba90a5659eb8bbc8b94f
luyaJ <1579618152@qq.com>
2018-01-01 22:35:36 +0800

    素数

---
+++ exercise_100/test12.py
@@ -0,0 +1,15 @@
+# 判断101-200之间有多少个素数，并输出所有素数
+# 判断方法：用一个数分别去除2到sqrt(这个数)，
+# 如果能被整除，则表明此数不是素数，反之是素数
+
+from math import sqrt
+l = []
+for i in range(101, 201):
+    for j in range(2, int(sqrt(i)+1)):
+        if i % j == 0:
+            break
+    else:
+        l.append(i)
+print(l)
+print("总数为：%d" % len(l))
+
