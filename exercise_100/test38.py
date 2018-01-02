c20c28983edd476d281b21fca1e927ea1cdcbb76
luyaJ <1579618152@qq.com>
2018-01-02 13:04:40 +0800

    文本颜色设置

---
+++ exercise_100/test38.py
@@ -0,0 +1,11 @@
+
+if __name__ == '__main__':
+    a = []
+    sum = 0.0
+    for i in range(3):
+        a.append([])
+        for j in range(3):
+            a[i].append(float(input("input number:")))
+    for i in range(3):
+        sum += a[i][i]
+    print(sum)
